471fa28c8665bdc5031f3f90d5fd9000aa3b11af
MuhammadNurShodiq <nursodik.2004@gmail.com>
2024-04-18 11:11:38 +0700

    aplikasi bank transfer dengan runtime configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,108 @@
-﻿internal class Program
+﻿using System.Text.Json;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string lang;
+        int threshold;
+        int low_fee;
+        int high_fee;
+        List<string> methods;
+        string confirmation;
+
+
+        BankInput bankInput = new BankInput();
+
+        
+    }
+
+    public class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set;}
+        public List<string> methods { get; set; }
+        public string en {  get; set; }
+        public string id { get; set; }
+
+        public BankTransferConfig() { }
+        public BankTransferConfig(string lang, int threshold, int low_fee, int high_fee, List<string> methods, string en, string id)
+        {
+            this.lang = lang;
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+            this.methods = methods;
+            this.en = en;
+            this.id = id;
+        }
+    }
+
+    public class BankInput
+    {
+        public BankTransferConfig bankTransferConfig;
+        public const String filepath = @"bank_transfer_config.json";
+
+        public BankInput()
+        {
+            try
+            {
+                ReadConfig();
+            }
+
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfig();
+            }
+        }
+
+        private BankTransferConfig ReadConfig()
+        {
+            string jsonData = File.ReadAllText(filepath);
+            bankTransferConfig = JsonSerializer.Deserialize<BankTransferConfig>(jsonData);
+            return bankTransferConfig;
+        }
+
+        private void SetDefault()
+        {
+            bankTransferConfig = new BankTransferConfig("en", 25000000, 6500, 15000, ["RTO (real-time)", "SKN", "RTGS", "BI FAST"], "yes", "ya");
+        }
+
+        private void WriteNewConfig()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            string JsonString = JsonSerializer.Serialize(bankTransferConfig, options);
+            File.WriteAllText(filepath, JsonString);
+        }
+
+        public void UbahData (string dataBaru)
+        {
+            bool dataValid = (dataBaru == "en" || dataBaru == "id");
+
+            if (dataBaru == null || !dataValid)
+            {
+                throw new ArgumentException();
+            }
+
+            if (dataValid)
+            {
+                bankTransferConfig.lang = dataBaru;
+
+                JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
+                {
+                    WriteIndented = true,
+                };
+
+                string updateData = JsonSerializer.Serialize(bankTransferConfig, jsonSerializerOptions);
+                File.WriteAllText (filepath, updateData);
+            }
+        }
     }
 }
